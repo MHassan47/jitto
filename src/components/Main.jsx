52fe83ed0498c77293c54774377cee0d85de098e
Mohamed <mhassankd35@gmail.com>
2023-03-22 06:11:24 -0500

    imports header and login components

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Login from "./Login";
+
+function Main() {
+  return (
+    <div className="">
+      <div className="h-28">
+        <Header />
+      </div>
+      <Login />
+    </div>
+  );
+}
+
+export default Main;
