f0c3b7ca8258a61f54c53894a9e483016552b560
Mohamed <mhassankd35@gmail.com>
2023-03-22 06:09:55 -0500

    adds router dom to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Main from "./components/Main";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Main />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
