0b77ab24bdc33ff752e3d80ec0473d14951f7bf4
Mohamed <mhassankd35@gmail.com>
2023-03-25 20:27:15 -0500

    adds buttons and selected styling to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 
-function Header() {
-  const [mode, setMode] = useState("Login");
+function Header({ setMode, mode }) {
+  console.log(mode);
   return (
     <div className="flex justify-center h-full bg-purple-500">
-      <div className="flex justify-around space-x-4 text-center items-center">
-        <div className="bg-white px-8 py-4">Login</div>
-        <div className="bg-white px-8 py-4">Detail</div>
+      <div className="flex justify-around space-x-4 text-center items-center ">
+        <div
+          className={`bg-white px-8 py-4 cursor-pointer text-lg font-semibold ${
+            mode === "Login" ? "border-b-8 border-yellow-300" : ""
+          }`}
+          onClick={() => setMode("Login")}
+        >
+          Login
+        </div>
+        <div
+          className={`bg-white px-8 py-4 cursor-pointer text-lg font-semibold ${
+            mode === "Detail" ? "border-b-8 border-yellow-300" : ""
+          }`}
+          onClick={() => setMode("Detail")}
+        >
+          Detail
+        </div>
       </div>
     </div>
   );
