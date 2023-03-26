4c15995a0454e2dc89686e241a55d0b7b950c2be
Mohamed <mhassankd35@gmail.com>
2023-03-25 20:28:07 -0500

    adds error state and conditional message

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,27 @@
 import { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 function Login() {
+  const navigate = useNavigate();
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const onSubmitHandler = () => {
+    if (!username || !password) {
+      return setError(true);
+    } else {
+      navigate("/landing");
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center w-5/6 ">
       <div className="flex-col justify-start w-1/3 mb-8">
         <div className="text-3xl font-bold">Welcome back</div>
         <div className="text-gray-500">Sign in to your account</div>
       </div>
+      {error && <div className="text-red-600">Field(s) cannot be empty</div>}
       <div className="flex justify-start w-1/3 mb-2">
         <div className="font-semibold">Username</div>
       </div>
@@ -43,7 +56,7 @@ function Login() {
       </div>
 
       <button
-        //   onClick={onSubmitHandler}
+        onClick={onSubmitHandler}
         className="bg-purple-600 h-10 w-40 text-white rounded-md mt-10 hover:bg-purple-700"
       >
         Sign In
