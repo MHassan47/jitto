0558cc1015782b9295564fe3e17c8629b0992eaa
Mohamed <mhassankd35@gmail.com>
2023-03-25 20:26:39 -0500

    fixes styling and adds item to categories

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
 import { IoIosArrowForward, IoIosArrowDown } from "react-icons/io";
+import Item from "./Item";
 function Category() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div
-      className="flex flex-row  bg-purple-600 p-6 text-xl text-white text-center font-semibold rounded-sm"
-      onClick={() => setIsOpen((prev) => !prev)}
-    >
-      {isOpen ? (
-        <IoIosArrowDown className="w-8 h-8 " />
-      ) : (
-        <IoIosArrowForward className="w-8 h-8 " />
+    <>
+      <div
+        className="flex flex-row  bg-purple-600 p-6 text-xl text-white text-center font-semibold rounded-sm"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        {isOpen ? (
+          <IoIosArrowDown className="w-8 h-8 " />
+        ) : (
+          <IoIosArrowForward className="w-8 h-8 " />
+        )}
+        <div className="flex-1">Detail</div>
+      </div>
+      {isOpen && (
+        <div className="flex flex-col my-6">
+          <Item>lorem ipsum</Item>
+          <Item>lorem ipsum</Item>
+        </div>
       )}
-      <div className="flex-1">Detail</div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Detail.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Category from "./Category";
+
+function Detail() {
+  return (
+    <div className="flex flex-col w-2/5">
+      <Category />
+      <Category />
+    </div>
+  );
+}
+
+export default Detail;
