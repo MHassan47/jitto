7819436e14ab55f9a74e10cb8ea7b1a96c34eb13
Mohamed <mhassankd35@gmail.com>
2023-03-26 19:37:29 -0500

    adds conditional rendering in category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { IoIosArrowForward, IoIosArrowDown } from "react-icons/io";
 import Item from "./Item";
-function Category() {
+import ProductList from "./ProductList";
+function Category({ category, productData }) {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div
-        className="flex flex-row  bg-purple-600 p-6 text-xl text-white text-center font-semibold rounded-sm"
+        className="flex flex-row  bg-purple-600 p-6 text-xl text-white text-center font-semibold rounded-sm mb-2"
         onClick={() => setIsOpen((prev) => !prev)}
       >
         {isOpen ? (
@@ -14,12 +15,18 @@ function Category() {
         ) : (
           <IoIosArrowForward className="w-8 h-8 " />
         )}
-        <div className="flex-1">Detail</div>
+        <div className="flex-1">{category ? category : "Detail"}</div>
       </div>
       {isOpen && (
         <div className="flex flex-col my-6">
-          <Item>lorem ipsum</Item>
-          <Item>lorem ipsum</Item>
+          {!category ? (
+            <div>
+              <Item>lorem ipsum</Item>
+              <Item>lorem ipsum</Item>
+            </div>
+          ) : (
+            <ProductList category={category} productData={productData} />
+          )}
         </div>
       )}
     </>
