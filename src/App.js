507ab9ee3192fa6409ff1e67133bbac7c2369c31
Mohamed <mhassankd35@gmail.com>
2023-03-26 19:36:19 -0500

    imports and creates cart route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart";
 import Landing from "./pages/Landing";
 import Main from "./pages/Main";
 
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Main />} />
         <Route path="/landing" element={<Landing />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
