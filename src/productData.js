7f9867a0ea029f745990b62bfd33f3ea5c8dc747
Mohamed <mhassankd35@gmail.com>
2023-03-26 19:37:04 -0500

    creates product and category data

---
+++ src/productData.js
@@ -0,0 +1,118 @@
+const productCategories = ["All", "Fruits", "Vegetables", "Grains", "Dairy"];
+
+const productData = [
+  {
+    name: "Lemon",
+    price: 9.99,
+    foodGroup: "Fruits",
+    weight: "1 kg",
+    image:
+      "https://img.freepik.com/premium-photo/lemon-isolated-white-background-natural-color_343283-947.jpg?w=360",
+  },
+  {
+    name: "Apple",
+    price: 1.25,
+    foodGroup: "Fruits",
+    weight: "1 kg",
+    image:
+      "https://img.freepik.com/premium-photo/red-apples-isolated-white-background_299651-65.jpg?w=2000",
+  },
+  {
+    name: "Banana",
+    price: 0.75,
+    foodGroup: "Fruits",
+    weight: "1 kg",
+    image:
+      "https://t3.ftcdn.net/jpg/03/03/66/56/360_F_303665638_AvUUcS4St4a3zcqA4FoW0tCDyyo5RwYR.jpg",
+  },
+  {
+    name: "Orange",
+    price: 1.0,
+    foodGroup: "Fruits",
+    weight: "1 kg",
+    image:
+      "https://img.pixers.pics/pho_wat(s3:700/FO/42/34/30/66/700_FO42343066_f6d79ad8e1607e64a395b0a2613a6355.jpg,700,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,650,jpg)/posters-orange-isolated-on-white-background.jpg.jpg",
+  },
+  {
+    name: "Strawberries",
+    price: 2.5,
+    foodGroup: "Fruits",
+    weight: "1 kg",
+    image:
+      "https://www.thermofisher.com/blog/food/wp-content/uploads/sites/5/2015/08/single_strawberry__isolated_on_a_white_background.jpg",
+  },
+  {
+    name: "Carrots",
+    price: 0.75,
+    foodGroup: "Vegetables",
+    weight: "1 kg",
+    image:
+      "https://t3.ftcdn.net/jpg/01/88/50/62/360_F_188506264_8MMq2BHoDlfoBYHDxiYsYn1KGKbGT38S.jpg",
+  },
+  //   {
+  //     name: "Broccoli",
+  //     price: 1.5,
+  //     foodGroup: "Vegetables",
+  //     weight: "1 lb",
+  //     image:
+  //       "https://www.seekpng.com/png/detail/149-1490467_png-free-download-broccoli-white-background-images-broccoli.png",
+  //   },
+  {
+    name: "Spinach",
+    price: 1.25,
+    foodGroup: "Vegetables",
+    weight: "1 kg",
+    image:
+      "https://img.freepik.com/premium-photo/close-upl-bundle-fresh-spinach-white-background_525574-1072.jpg",
+  },
+  {
+    name: "Brown Rice",
+    price: 2.0,
+    foodGroup: "Grains",
+    weight: "1 kg",
+    image:
+      "https://thumbs.dreamstime.com/b/bowl-brown-rice-white-background-151149347.jpg",
+  },
+  //   {
+  //     name: "Quinoa",
+  //     price: 3.0,
+  //     foodGroup: "Grains",
+  //     weight: "1 lb",
+  //     image:
+  //       "https://img.freepik.com/premium-photo/quinoa-bowl-white-background_55883-11400.jpg?w=2000",
+  //   },
+  {
+    name: "Oats",
+    price: 1.5,
+    foodGroup: "Grains",
+    weight: "1 kg",
+    image:
+      "https://t4.ftcdn.net/jpg/02/41/02/65/360_F_241026550_40XK5QLDoAGiLwXFISGfnJnpTJ7DhUKc.jpg",
+  },
+  {
+    name: "Whole Wheat Bread",
+    price: 2.5,
+    foodGroup: "Grains",
+    weight: "1 kg",
+    image:
+      "https://media.istockphoto.com/id/157587362/photo/detailed-close-up-of-sliced-grain-bread-on-white-background.jpg?s=612x612&w=0&k=20&c=nr5f0Mb3Dx9RP_LmKKwlo6IlzDebfIdCbnERsnoSG94=",
+  },
+  {
+    name: "Milk",
+    price: 2.0,
+    foodGroup: "Dairy",
+    weight: "4 L",
+    image:
+      "https://media.istockphoto.com/id/998508938/photo/milk-bottle-on-white-background.jpg?s=170667a&w=0&k=20&c=Fh6awJU6OdoS41LLgKESy0Ujn15eUMRk3CeMoFz1i5Y=",
+  },
+  {
+    name: "Yogurt",
+    price: 1.5,
+    foodGroup: "Dairy",
+    weight: "500 g",
+    image:
+      "https://storage.googleapis.com/images-sof-prd-9fa6b8b.sof.prd.v8.commerce.mi9cloud.com/product-images/detail/00059161910109.jpg",
+  },
+];
+
+export { productCategories, productData };
